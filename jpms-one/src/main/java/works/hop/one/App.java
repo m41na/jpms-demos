0715aad162998cb23f51ae6e037fc2d121c3b319
Stephen Maina <m41na@yahoo.com>
2018-10-20 16:12:37 -0500

    first commit

---
+++ jpms-one/src/main/java/works/hop/one/App.java
@@ -0,0 +1,13 @@
+package works.hop.one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/works/hop/jpms/App.java
@@ -0,0 +1,13 @@
+package works.hop.jpms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
